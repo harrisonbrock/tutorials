1ed667190d058462c12a3bf51607044089049d53
Harrison Brock <harrison@harrisonbrock.com>
2020-01-12 20:48:53 -0500

    Added Trim String demo.

---
+++ core-strings/src/blog/harrisonbrock/TrimString.java
@@ -0,0 +1,10 @@
+package blog.harrisonbrock;
+
+public class TrimString {
+    public static void main(String[] args) {
+
+        // Remove Leading and Trailing Spaces From a String
+        String name = " Harry ".trim();
+        System.out.println(name);
+    }
+}
